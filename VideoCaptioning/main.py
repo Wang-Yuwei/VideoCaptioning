d5dfd859718a7ce4bd118aa32a72addf06286d22
Wang-Yuwei <yuw267@eng.ucsd.edu>
2017-05-23 21:05:55 -0700

    Seperate train and test

--- VideoCaptioning/main.py
+++ VideoCaptioning/main.py
@@ -4,15 +4,12 @@ from youtube import preprocess_data, process_dict
 from youtube_generator import YouTubeGenerator
 from train import *
 
-'''
-preprocess_data('youtubeclips/video-descriptions.csv',
-                'youtubeclips/youtube_mapping.txt',
-                'result.txt')
-'''
-word2idx, idx_to_word = process_dict('result.txt', 'dict.txt', 'index.txt')
+preprocess_data('youtubeclips')
 
-batch_size = 2
-generator = YouTubeGenerator('index.txt', 'features', batch_size)
-#need to make a new folder, called save
-train(generator, save_path='save')
-generation(generator, save_path='save', videos=['vid1'], idx_to_word=idx_to_word)
+word2idx, idx_to_word = process_dict('youtubeclips')
+
+#batch_size = 2
+#generator = YouTubeGenerator('index.txt', 'features', batch_size)
+##need to make a new folder, called save
+#train(generator, save_path='save')
+#generation(generator, save_path='save', videos=['vid1'], idx_to_word=idx_to_word)
--- VideoCaptioning/youtube.py
+++ VideoCaptioning/youtube.py
@@ -1,7 +1,19 @@
-def preprocess_data(description_filename, mapping_filename, output_filename):
+from utilities import checked_mkdirs
+
+def preprocess_data(path):
+    checked_mkdirs(path + '/result')
+    description_filename = path + '/video-descriptions.csv'
+    mapping_filename = path + '/youtube_mapping.txt'
+    train_output_filename = path + '/result/train-result.txt'
+    test_output_filename = path + '/result/test-result.txt'
+    train_filename = path + '/train.txt'
+    train_list = []
+    with open(train_filename) as train:
+        train_list = list(map(lambda x: x.strip(' \t\n\r'), train.readlines()))
     with open(description_filename, 'r', encoding='utf8') as desc_file, \
         open(mapping_filename, encoding='utf8') as mapping_file, \
-        open(output_filename, 'w+', encoding='utf8') as output:
+        open(train_output_filename, 'w+', encoding='utf8') as train_output, \
+        open(test_output_filename, 'w+', encoding='utf8') as test_output:
         youtube_map = {}
         for line in mapping_file.readlines():
             [url, name] = line.split(' ')
@@ -12,12 +24,23 @@ def preprocess_data(description_filename, mapping_filename, output_filename):
             sentence = fields[-1].strip(' \t\n\r')[:-1]
             if not url in youtube_map or len(sentence.split(' ')) > 20:
                 continue
-            print(youtube_map[url] +' ' + sentence, file = output)
+            name = youtube_map[url]
+            if name in train_list:
+                print(youtube_map[url] +' ' + sentence, file = train_output)
+            else:
+                print(youtube_map[url] +' ' + sentence, file = test_output)
 
-def process_dict(sentence_filename, diction_filename, output_filename):
-    with open(sentence_filename, encoding='utf8') as sentence_file, \
+def process_dict(path):
+    train_filename = path + '/result/train-result.txt'
+    test_filename = path + '/result/test-result.txt'
+    diction_filename = path + '/result/dict.txt'
+    train_output_filename = path + '/result/train-index.txt'
+    test_output_filename = path + '/result/test-index.txt'
+    with open(train_filename, encoding='utf8') as train_file, \
+        open(test_filename, encoding='utf8') as test_file, \
         open(diction_filename, 'w+', encoding='utf8') as diction_output, \
-        open(output_filename, 'w+', encoding='utf8') as output:
+        open(train_output_filename, 'w+', encoding='utf8') as train_output, \
+        open(test_output_filename, 'w+', encoding='utf8') as test_output:
         dictionary = {}
         dictionary['<begin>'] = 0
         dictionary['<end>'] = 1
@@ -25,26 +48,42 @@ def process_dict(sentence_filename, diction_filename, output_filename):
         dict_list = ['<begin>', '<end>', '<padding>']
         words_list = []
         max_length = 0
-        for line in sentence_file.readlines():
-            words = line.split(' ')
-            words_list.append(words)
-            max_length = max(max_length, len(words))
+        def process_word(input):
+            nonlocal max_length
+            nonlocal words_list
+            nonlocal dict_list
+            nonlocal dictionary
+            for line in input.readlines():
+                words = line.split(' ')
+                words = list(filter(lambda x: x != '',
+                                    map(lambda x: x.strip(' \t\n\r.!'), words)))
+                words_list.append(words)
+                max_length = max(max_length, len(words))
+        def process_input(input, output):
+            nonlocal max_length
+            nonlocal words_list
+            nonlocal dict_list
+            nonlocal dictionary
+            for words in words_list:
+                id = words[0]
+                words = words[1: ]
+                row = [id, 0]
+                if len(words) < 2:
+                    continue
+                for word in words:
+                    if word not in dictionary:
+                        dict_list.append(word)
+                        dictionary[word] = len(dict_list) - 1
+                    row.append(dictionary[word])
+                row.append(1)
+                row = row + [2] * (max_length - len(row))
+                print(' '.join(map(str, row)), file = output)
+        process_word(train_file)
+        process_word(test_file)
         max_length += 2
-        print('The max length of sentence is %d. ' % max_length)
-        for words in words_list:
-            id = words[0]
-            words = words[1: ]
-            row = [id, 0]
-            if len(words) < 2:
-                continue
-            for word in words:
-                if word not in dictionary:
-                    dict_list.append(word)
-                    dictionary[word] = len(dict_list) - 1
-                row.append(dictionary[word])
-            row.append(1)
-            row = row + [2] * (max_length - len(row))
-            print(' '.join(map(str, row)), file = output)
+        print('The max length of sentence is %d. ' % max_length)    
+        process_input(train_file, train_output)
+        process_input(test_file, test_output)
         for word in dict_list:
             print(word, file = diction_output)
     return dictionary, dict_list
