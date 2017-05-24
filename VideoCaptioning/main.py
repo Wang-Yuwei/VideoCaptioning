2beaf786868367a76a4f3ec958e087d3e1ab3b73
Wang-Yuwei <yuw267@eng.ucsd.edu>
2017-05-23 21:17:39 -0700

    Prepare to train.

--- VideoCaptioning/main.py
+++ VideoCaptioning/main.py
@@ -3,13 +3,13 @@ import numpy
 from youtube import preprocess_data, process_dict
 from youtube_generator import YouTubeGenerator
 from train import *
+import tensorflow as tf
 
 preprocess_data('youtubeclips')
-
 word2idx, idx_to_word = process_dict('youtubeclips')
-
-#batch_size = 2
-#generator = YouTubeGenerator('index.txt', 'features', batch_size)
-##need to make a new folder, called save
-#train(generator, save_path='save')
-#generation(generator, save_path='save', videos=['vid1'], idx_to_word=idx_to_word)
+batch_size = 200
+generator = YouTubeGenerator('youtubeclips/result/train-index.txt', 'features', batch_size)
+#need to make a new folder, called save
+train(generator, save_path='models')
+#tf.reset_default_graph()
+#generation(generator, save_path='models', videos=['vid1'], idx_to_word=idx_to_word)
